a838e0dbc4e073756c7ba87c2e47cf6f8fffc2c7
nz <nzampet@clemson.edu>
2024-11-18 18:44:02 -0500

    Fixed heatmap and added legend to it

--- heatmap.js
+++ heatmap.js
@@ -9,16 +9,10 @@ Promise.all([
   d3.json("Neighborhood_Statistical_Area_(NSA)_Boundaries.geojson"), // GeoJSON file
   d3.csv("fixedDataset.csv") // CSV file
 ]).then(([geoData, csvData]) => {
-
-  //console.log("Sample Geometry:", geoData.features[0].geometry);
-  //console.log("Sample Geometry Coordinates:", geoData.features[0].geometry.coordinates[0]);
-
-
   // Create a Mercator projection that fits the data
-  const projection = d3.geoMercator()
+  const projection = d3.geoIdentity()
+    .reflectY(true)
     .fitExtent([[20, 20], [width - 20, height - 20]], geoData)
-    .scale(150000)
-    .center([-76.612, 39.290])
 
   const path = d3.geoPath().projection(projection);
 
@@ -36,26 +30,43 @@ Promise.all([
     .domain([0, maxCalls])
     .interpolator(d3.interpolateBlues);
 
-  // Draw map
+  //Draw map
   svg.append("g")
     .selectAll(".Neighborhood")
     .data(geoData.features)
     .enter()
     .append("path")
     .attr("class", "Neighborhood")
-    .attr("fill", "none")
     .attr("d", d => path(d))
-    // .attr("fill", d => {
-    //   const calls = callCountLookup[d.properties.Name] || 0;
-    //   return colorScale(calls);
-    // }) // This part causes the problem, map renders correctly when it is commented out
-    //Can't figure out exactly why it breaks though
+    .attr("fill", d => {
+      const calls = callCountLookup[d.properties.Name] || 0;
+      return colorScale(calls);
+    })
     .attr("stroke", "black")
     .append("title")
     .text(d => `${d.properties.Name}: ${callCountLookup[d.properties.Name] || 0} calls`);
 
-  // Debug paths
-  //console.log("Sample Path:", path(geoData.features[0]));
+  //Legend
+  const legend = svg.append("g")
+    .attr("transform", `translate(${width - 100}, ${height - 300})`);
+
+  const legendScale = d3.scaleLinear()
+    .domain([0, d3.max([...callCounts.values()])])
+    .range([0, 100]);
+
+  const legendAxis = d3.axisRight(legendScale).ticks(5);
+  
+  legend.selectAll("rect")
+    .data(d3.range(0, 1, 0.1))
+    .enter().append("rect")
+    .attr("y", (d, i) => i * 10)
+    .attr("width", 10)
+    .attr("height", 10)
+    .attr("fill", d => colorScale(d * d3.max([...callCounts.values()])));
+
+  legend.append("g")
+    .attr("transform", "translate(10, 0)")
+    .call(legendAxis);
 }).catch(error => {
   console.error("Error loading data:", error);
 });
